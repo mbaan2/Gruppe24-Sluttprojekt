86df9e4c832e7c7bccf007538699cb9c9a797d9a
anafvana <56302061+anafvana@users.noreply.github.com>
2020-03-30 16:57:13 +0200

    Kotlin library fixed.

--- Sluttprosjekt/src/main/java/module-info.java
+++ Sluttprosjekt/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Gruppe24.OSLOMET {
     requires javafx.controls;
     requires javafx.fxml;
+    requires kotlin.stdlib;
 
     opens Gruppe24.OSLOMET to javafx.fxml;
     exports Gruppe24.OSLOMET;
