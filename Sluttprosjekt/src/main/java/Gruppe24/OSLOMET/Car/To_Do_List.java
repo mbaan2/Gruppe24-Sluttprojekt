035ed83fc9770a2107948da71529b2c454a98144
anafvana <56302061+anafvana@users.noreply.github.com>
2020-03-30 11:58:01 +0200

    To-do list update

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/Car/To_Do_List.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/Car/To_Do_List.java
@@ -2,8 +2,9 @@
     1) Fix BuidingNewCar/CarBuild relation MARK
     2) Build file import system ANA
     3) Own thread for superuser upload (waiting time)
-    4) Tests for data validation
-    5) Save button for user
-    6) History of built cars
-    7) Polish look
+    4) Develop user interface
+    5) Tests for data validation
+    6) Save button for user
+    7) History of built cars
+    8) Polish look
 */
