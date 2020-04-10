deec6f70f0b330339f35d37f72df51865a463a32
anafvana <56302061+anafvana@users.noreply.github.com>
2020-04-10 18:17:23 +0200

    Tiny update: changed to-do list location and updated contents.

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
@@ -1,10 +1,8 @@
 /*
     3)      SuperUser is create Thread and several try/catch has to be added
     5)      Tests for data validation
-    6)      Save button for user
     7)      History of built cars
     8)      Polish look
-    9)      Export to Excel
     10)     JUnit testing
     11)     Creating our own exceptions classes
     12)     No duplicate values in admin add buttons
