b82a0814658976d49160cd2b3ba9785f3c0339f6
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2020-04-27 18:07:08 +0200

    Made some small changes. /shrug

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/SuperUserCarView_Controller.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/SuperUserCarView_Controller.java
@@ -37,9 +37,15 @@ public class SuperUserCarView_Controller implements Initializable {
     ObservableList<NewCar> filteredList = FXCollections.observableArrayList();
     ObservableList<String> choiceBoxList = FXCollections.observableArrayList();
     ObservableList<String> fuelList = FXCollections.observableArrayList();
+    ObservableList<String> wheelList = FXCollections.observableArrayList();
+    ObservableList<String> colorList = FXCollections.observableArrayList();
 
     @FXML
     void filterCars() {
+        for(int i = 0; i < carList.size(); i++) {
+            System.out.println(carList.get(i).getFuel().getName() + " " + carList.get(i).getFuel().getCost());
+        }
+
         filteredList.clear();
         filterLbl.setText("");
 
@@ -149,6 +155,18 @@ public class SuperUserCarView_Controller implements Initializable {
         String gasFuel = "Gasoline Car";
         fuelList.addAll(dieselFuel, electricFuel, gasFuel);
 
+        String bigWheels = "Big wheels";
+        String smallWheels = "Small wheels";
+        String mediumWheels = "Medium wheels";
+        wheelList.addAll(smallWheels, mediumWheels, bigWheels);
+
+        String redColor = "Red";
+        String blueColor = "Blue";
+        String yellowColor = "Yellow";
+        String blackColor = "Black";
+        String greenColor = "Green";
+        colorList.addAll(redColor, blueColor, yellowColor, blackColor, greenColor);
+
         for(int i = 0; i < maxAntallAddons; i ++) {
             TableColumn<NewCar, String> tc = new TableColumn<>("Addon " + (i + 1));
             addon.getColumns().add(tc);
@@ -165,19 +183,46 @@ public class SuperUserCarView_Controller implements Initializable {
 
             name.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getName()));
             name.setCellFactory(TextFieldTableCell.forTableColumn());
-            user.setOnEditCommit(event -> event.getRowValue().setName(event.getNewValue()));
+            name.setOnEditCommit(event -> event.getRowValue().setName(event.getNewValue()));
 
-            fuel.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getFuel().getName() + " (" + car.getValue().getFuel().getCost() + " kr)"));
+            fuel.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getFuel().getName()));
             fuel.setCellFactory(ComboBoxTableCell.forTableColumn(fuelList));
-            fuel.setOnEditCommit(event -> event.getRowValue().setFuel(new Carparts(event.getNewValue(), event.getRowValue().getFuel().getCost())));
+            fuel.setOnEditCommit(event -> {
+                if(event.getNewValue().equals("Diesel Car")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 20000));
+                } else if(event.getNewValue().equals("Electric Car")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 17500));
+                } else if(event.getNewValue().equals("Gasoline Car")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 30000));
+                }
+            });
 
-            wheels.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getWheels().getName() + " (" + car.getValue().getWheels().getCost() + " kr)"));
-            wheels.setCellFactory(TextFieldTableCell.forTableColumn());
-            wheels.setOnEditCommit(event -> event.getRowValue().setWheels(new Carparts(event.getNewValue(), event.getRowValue().getWheels().getCost())));
-
-            color.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getColor().getName() + " (" + car.getValue().getColor().getCost() + " kr)"));
-            color.setCellFactory(TextFieldTableCell.forTableColumn());
-            color.setOnEditCommit(event -> event.getRowValue().setColor(new Carparts(event.getNewValue(), event.getRowValue().getColor().getCost())));
+            wheels.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getWheels().getName()));
+            wheels.setCellFactory(ComboBoxTableCell.forTableColumn(wheelList));
+            wheels.setOnEditCommit(event -> {
+                if(event.getNewValue().equals("Small wheels")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 0));
+                } else if(event.getNewValue().equals("Medium wheels")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 2500));
+                } else if(event.getNewValue().equals("Big wheels")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 5000));
+                }
+            });
+            color.setCellValueFactory(car -> new SimpleStringProperty(car.getValue().getColor().getName()));
+            color.setCellFactory(ComboBoxTableCell.forTableColumn());
+            color.setOnEditCommit(event -> {
+                if(event.getNewValue().equals("Red")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 5000));
+                } else if(event.getNewValue().equals("Blue")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 2500));
+                } else if(event.getNewValue().equals("Yellow")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 2500));
+                } else if(event.getNewValue().equals("Black")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 0));
+                } else if(event.getNewValue().equals("Green")) {
+                    event.getRowValue().setFuel(new Carparts(event.getNewValue(), 3500));
+                }
+            });
 
             for (int j = 0; j < maxAntallAddons; j++) {
                 int finalJ = j;
