bf0dcacb49eccf4ee171ebc3efa2a9b8fa9921b8
mbaan2 <mbaan2@gmail.com>
2020-04-28 14:21:38 +0200

    Adding name to the car fixed

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/Summary_Controller.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/Summary_Controller.java
@@ -35,67 +35,14 @@ public class Summary_Controller {
     private Button btnBuildCar;
 
     @FXML
+    private Button btnSaveCarToText;
+
+    @FXML
     private Label lblCarComponents;
 
     @FXML
     private Button quinaryButton;
 
-    @FXML
-    void btnSaveCar(ActionEvent event) {
-        lblCarName.setVisible(true);
-        btnNameCar.setVisible(true);
-        btnSaveCar.setDisable(true);
-        btnSaveCar.setLayoutX(314.0);
-
-        Carparts fuel = new Carparts("Diesel", 1000);
-        Carparts wheels = new Carparts("Big wheels", 1000);
-        Carparts color = new Carparts("Red", 1000);
-
-
-        Carparts gps = new Carparts("GPS", 1000);
-        Carparts spoiler = new Carparts("Spoiler", 1000);
-        CarCategory add_ones = new CarCategory("Add Ones");
-        add_ones.add(gps);
-        add_ones.add(spoiler);
-
-
-        NewCar Car1 = new NewCar();
-        Car1.setUser("123");
-        Car1.setFuel(fuel);
-        Car1.setWheels(wheels);
-        Car1.setColor(color);
-        Car1.setAddons(add_ones);
-
-        List<NewCar> list = new ArrayList<>();
-        list.add(Car1);
-        list.add(Car1);
-
-
-        ArrayList<NewCar> list2 = new ArrayList<>();
-
-        try {
-            //TO SAVE THE INITIAL LIST
-            FileSaverJobj.SavingCarArray(StandardPaths.carsPath, list);
-            FileSaverJobj.addingOnlyOneCarObject(StandardPaths.carsPath, App.car);
-            list2 = FileOpenerJobj.openingCarArray(StandardPaths.carsPath);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        for(int i = 0; i<list2.size(); i++){
-            System.out.println(list2.get(i).getUser());
-            System.out.println(list2.get(i).getAddons().getElement(0).getName());
-        }
-
-
-
-        /*
-        Code to set the initial two cars of the cars.jobj file
-        THIS HAS TO BE REMOVED LATER
-        */
-
-
-    }
 
     @FXML
     void btnBuildCar(ActionEvent event) {
@@ -105,18 +52,84 @@ public class Summary_Controller {
 
         btnBuildCar.setDisable(true);
         btnBuildCar.setLayoutX(226.0);
-        btnSaveCar.setVisible(true);
+        btnNameCar.setVisible(true);
+        lblCarName.setVisible(true);
+
     }
 
     @FXML
     void btnNameCar(ActionEvent event) {
+        // We need a function here that test if the name already exist
+        // If we have time it would be could to add a random name generator
+        String nameCar = lblCarName.getText();
+        if (!nameCar.equals("")) {//&& nameCar.exist();{
+            App.car.setName(lblCarName.getText());
+            btnNameCar.setDisable(true);
+            btnSaveCarToText.setVisible(true);
+
+            //Code has to be removed when program is done!
+            Carparts fuel = new Carparts("Diesel", 1000);
+            Carparts wheels = new Carparts("Big wheels", 1000);
+            Carparts color = new Carparts("Red", 1000);
+
+
+            Carparts gps = new Carparts("GPS", 1000);
+            Carparts spoiler = new Carparts("Spoiler", 1000);
+            CarCategory add_ones = new CarCategory("Add Ones");
+            add_ones.add(gps);
+            add_ones.add(spoiler);
+
+
+            NewCar Car1 = new NewCar();
+            Car1.setUser("123");
+            Car1.setFuel(fuel);
+            Car1.setWheels(wheels);
+            Car1.setColor(color);
+            Car1.setAddons(add_ones);
+
+            List<NewCar> list = new ArrayList<>();
+            list.add(Car1);
+            list.add(Car1);
+
+
+            // Used for testing
+            //ArrayList<NewCar> list2 = new ArrayList<>();
+
+            try {
+                //TO SAVE THE INITIAL LIST
+                FileSaverJobj.SavingCarArray(StandardPaths.carsPath, list);
+                FileSaverJobj.addingOnlyOneCarObject(StandardPaths.carsPath, App.car);
+                //list2 = FileOpenerJobj.openingCarArray(StandardPaths.carsPath);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            System.out.println("Car is added to the list.");
+
+            /*
+            Used for testing
+            for(int i = 0; i<list2.size(); i++){
+                System.out.println(list2.get(i).getUser());
+                System.out.println(list2.get(i).getAddons().getElement(0).getName());
+            }
+
+             */
+        } else{
+            System.err.println("Please give the car a name");
+            // System.err.println("Name is already taken");
+        }
+
+    }
+
+    @FXML
+    void btnSaveCarToText(ActionEvent event) {
         FileSaverTxt fs = new FileSaverTxt();
         try{
             fs.saveTxtFile(App.car);
         }catch (IOException e){
             e.printStackTrace();
         }
-        btnSaveCar.setDisable(false);
+        btnNameCar.setDisable(false);
     }
 
 
