5a42994c1b9aee255b68c86d7fce4533891a3ab5
anafvana <56302061+anafvana@users.noreply.github.com>
2020-04-30 19:12:09 +0200

    Todo updated

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
@@ -1,12 +1,17 @@
+//TODO
 /*
-    3)      SuperUser is create Thread and several try/catch has to be added
-    5)      Tests for data validation
-    6)      Save button for user
-    7)      History of built cars
-    8)      Polish look
-    10)     JUnit testing
-    11)     Creating our own exceptions classes
-    12)     No duplicate values in admin add buttons
-    13)     Function to check if username already exist
+    1)      Add thread for superuser when changing carparts             HIGH PRIORITY
+    2)      Add filters and ordering for superuser                      LOW PRIORITY
+    3)      Double check the crashes and try/catches                    MEDIUM PRIORITY
+    4)      Tests for data validation                                   HIGH PRIORITY       Ana
+    5)      Edit buttons for carparts in superuser                      HIGH PRIORITY
+    6)      JUnit testing                                               MEDIUM PRIORITY
+    7)      Make fuel buttons save to file and make them editable       HIGH PRIORITY       Mark
+    8)      Polish look                                                 MEDIUM PRIORITY     Øyvind
+    9)      Undo button per car                                         LOW PRIORITY
+    10)     Delete button for each car                                  HIGH PRIORITY       Ana
+    11)     Creating our own exceptions classes                         MEDIUM PRIORITY
+    12)     No duplicate values in admin add buttons                    HIGH PRIORITY
+    13)     Function to check if username already exists                HIGH PRIORITY       Øyvind
 
 */
