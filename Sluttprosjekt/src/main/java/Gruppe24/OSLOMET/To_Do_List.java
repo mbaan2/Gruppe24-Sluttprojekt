ec00402612df5d30156763aeb13f5716c4432589
anafvana <56302061+anafvana@users.noreply.github.com>
2020-05-05 15:45:04 +0200

    hIGH

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
@@ -2,7 +2,7 @@
 
 /*    1)      Add thread for superuser when changing carparts             done
       2)      Add filters and ordering for superuser                      done                                                      */
-//    3)      Double check the crashes and try/catches                    MEDIUM PRIORITY
+//    3)      Double check the crashes and try/catches                    HIGH PRIORITY
 /*    4)      Tests for data validation                                   done                  Ana
       5)      Edit buttons for carparts in superuser                      done
       6)      JUnit testing                                               done
@@ -10,7 +10,7 @@
       8)      Polish look                                                 done                Øyvind                                */
 //    9)      Undo button per car                                         LOW PRIORITY
 //    10)     Delete button for each car                                  HIGH PRIORITY       Ana
-//    11)     Creating our own exceptions classes                         MEDIUM PRIORITY
+//    11)     Creating our own exceptions classes                         HIGH PRIORITY
 /*    12)     No duplicate values in admin add buttons                    done
       13)     Function to check if username already exists                done                Øyvind                                */
 //    14)     Total cost column at the end                                HIGH PRIORITY       Ana
