b30113ec54dc242a2d74e7f5e5f29247c2637e1f
anafvana <56302061+anafvana@users.noreply.github.com>
2020-05-05 19:26:23 +0200

    Added price only to set label

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
@@ -3,17 +3,17 @@
 /*    1)      Add thread for superuser when changing carparts             done
       2)      Add filters and ordering for superuser                      done                                                      */
 //    3)      Double check the crashes and try/catches                    HIGH PRIORITY
-/*    4)      Tests for data validation                                   done                  Ana
+/*    4)      Tests for data validation                                   done
       5)      Edit buttons for carparts in superuser                      done
       6)      JUnit testing                                               done
-      7)      Make fuel buttons save to file and make them editable       done                Mark
-      8)      Polish look                                                 done                Øyvind                                */
+      7)      Make fuel buttons save to file and make them editable       done
+      8)      Polish look                                                 done                                                      */
 //    9)      Undo button per car                                         LOW PRIORITY
 //    10)     Delete button for each car                                  HIGH PRIORITY       Ana
 //    11)     Creating our own exceptions classes                         HIGH PRIORITY
 /*    12)     No duplicate values in admin add buttons                    done
-      13)     Function to check if username already exists                done                Øyvind                                */
-//    14)     Total cost column at the end                                HIGH PRIORITY       Ana
+      13)     Function to check if username already exists                done
+      14)     Total cost column at the end                                done                                                      */
 //    15)     Test with Netbeans and Eclipse                              HIGH PRIORITY
 //    16)     Include text file talking about the possible JUnit problem and how to fix
 //    17)     Add to login screen admin/admin                             HIGH PRIORITY
