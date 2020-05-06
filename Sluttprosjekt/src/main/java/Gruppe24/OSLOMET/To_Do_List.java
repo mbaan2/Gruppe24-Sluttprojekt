c5524f7e41901a1d3d92e9d4936e3f9b918a956c
mbaan2 <mbaan2@gmail.com>
2020-05-06 07:58:32 +0200

    Updated To-Do-List

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/To_Do_List.java
@@ -1,26 +1,42 @@
 //TODO
+/*
+      3)      Double check the crashes and try/catches                    HIGH PRIORITY
+      3.5)    Removing all the System.err.messages and replace them
+              with feedback to the user                                   HIGH PRIORITY
+      11)     Creating our own exceptions classes                         HIGH PRIORITY
+      15)     Test with Netbeans and Eclipse                              HIGH PRIORITY
+      16)     Include text file talking about the
+              possible JUnit problem and how to fix                       HIGH PRIORITY
+      18)     Remove the overwriting code                                 HIGH PRIORITY
+      20)     Fix example data                                            HIGH PRIORITY
+      21)     Save file to txt file-not-selected crash: FIX               HIGH PRIORITY
+      23)     Tableview on thread                                         HIGH PRIORITY
+      24)     Fuck up binary and see if it works (reset all binary files) HIGH PRIORITY
+      25)     ValidName.functions if() statements in the program
+              dont have a else function (feedback to user yet)            HIGH PRIORITY
 
-/*    1)      Add thread for superuser when changing carparts             done
-      2)      Add filters and ordering for superuser                      done                                                      */
-//    3)      Double check the crashes and try/catches                    HIGH PRIORITY
-/*    4)      Tests for data validation                                   done
+
+      9)      Undo button per car                                         LOW PRIORITY
+      19)     Option to edit a specific car                               LOW PRIORITY        Mark
+*/
+
+
+//TODO - Done
+
+/*
+      1)      Add thread for superuser when changing carparts             done
+      2)      Add filters and ordering for superuser                      done
+      4)      Tests for data validation                                   done
       5)      Edit buttons for carparts in superuser                      done
       6)      JUnit testing                                               done
       7)      Make fuel buttons save to file and make them editable       done
-      8)      Polish look                                                 done                                                      */
-//    9)      Undo button per car                                         LOW PRIORITY
-/*    10)     Delete button for each car                                  done                                                      */
-//    11)     Creating our own exceptions classes                         HIGH PRIORITY
-/*    12)     No duplicate values in admin add buttons                    done
+      8)      Polish look                                                 done
+      10)     Delete button for each car                                  done
+      12)     No duplicate values in admin add buttons                    done
       13)     Function to check if username already exists                done
-      14)     Total cost column at the end                                done                                                      */
-//    15)     Test with Netbeans and Eclipse                              HIGH PRIORITY
-//    16)     Include text file talking about the possible JUnit problem and how to fix
-/*    17)     Add to login screen admin/admin                             done                                                      */
-//    18)     Remove the overwriting code                                 HIGH PRIORITY
-//    19)     Option to edit a specific car                               LOW PRIORITY        Mark
-//    20)     Fix example data                                            HIGH PRIORITY
-//    21)     Save file to txt file-not-selected crash: FIX               HIGH PRIORITY
-//    22)     Feedback to the user on sout, but not on GUI. FIX           done
-//    23)     Tableview on thread                                         HIGH PRIORITY
-//    24)     Fuck up binary and see if it works (reset all binary files) HIGH PRIORITY
+      14)     Total cost column at the end                                done
+      22)     Feedback to the user on sout, but not on GUI. FIX           done
+      17)     Add to login screen admin/admin                             done
+
+
+ */
