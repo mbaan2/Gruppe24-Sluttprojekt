355c5296c05db66e524e1a7028f1186dd0f4133c
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2020-05-08 19:47:21 +0200

    Added something I overwrote earlier

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/SuperUserClasses/TableView/TableViewCreation.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/SuperUserClasses/TableView/TableViewCreation.java
@@ -26,6 +26,7 @@ import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.io.IOException;
@@ -273,7 +274,15 @@ public class TableViewCreation {
                     }
                     if (!hasMatch){
                         Button unmatchedAddon = new Button();
+                        Tooltip deleteInfo = new Tooltip("Pressing this button will delete the addon.\nThis action cannot be undone.");
+                        unmatchedAddon.setDisable(false);
+                        deleteInfo.setWrapText(true);
+                        deleteInfo.setShowDelay(Duration.millis(5));
+                        deleteInfo.setStyle("&#10#10");
+                        unmatchedAddon.setTooltip(deleteInfo);
+
                         if(!tableviewSuperUser){
+                            deleteInfo.hide();
                             unmatchedAddon.setDisable(true);
                         }
                         unmatchedAddon.setText(car.getValue().getAddons().getElement(j).getName());
