2f989df4cc0c1622b4dd09104512f56289e2afec
mbaan2 <mbaan2@gmail.com>
2020-05-10 19:29:22 +0200

    removing some code

--- Sluttprosjekt/src/test/java/Gruppe24/OSLOMET/DataValidation/ValidPriceTest.java
+++ Sluttprosjekt/src/test/java/Gruppe24/OSLOMET/DataValidation/ValidPriceTest.java
@@ -15,7 +15,7 @@ class ValidPriceTest{
         assertTrue(ValidPrice.validPrice(10000000));
         assertTrue(ValidPrice.validPrice(0));
         assertTrue(ValidPrice.validPrice(0));
-        
+
         assertFalse(ValidPrice.validPrice(-1));
         assertFalse(ValidPrice.validPrice(-900));
     }
