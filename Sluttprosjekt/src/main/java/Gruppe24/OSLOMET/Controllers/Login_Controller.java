7f05361eb4d5b9fb520c91cefb2107bc35b343de
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2020-05-10 17:57:53 +0200

    Login fix

--- Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/Controllers/Login_Controller.java
+++ Sluttprosjekt/src/main/java/Gruppe24/OSLOMET/Controllers/Login_Controller.java
@@ -99,7 +99,7 @@ public class Login_Controller implements Initializable {
                 }
                 if (passwordTxt.getText().isEmpty()){
                     passwordError.setText("Enter a password");
-                } else if (!userBase.containsValue(passwordTxt.getText())){
+                } else if (!userBase.get(usernameTxt.getText()).equals(passwordTxt.getText())){
                     passwordError.setText("Wrong password");
                 }
 
